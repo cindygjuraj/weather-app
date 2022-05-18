a7df4b8eafeaf9c87b0d35d3ee21f2979b9eb545
cindygjuraj <102333976+cindygjuraj@users.noreply.github.com>
2022-05-18 19:36:48 -0400

    Add Forecast Temp

--- index.js
+++ index.js
@@ -16,15 +16,39 @@ let days = [
 ];
 let day = days[now.getDay()];
 let h3 = document.querySelector("h3");
-h3.innerHTML = day + " " + hour + ":" + minutes;
+h3.innerHTML = day + "<br>" + hour + ":" + minutes;
+
+function displayForecast () {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function(day) {
+  forecastHTML = 
+    forecastHTML + `
+        <div class="col-2">
+          <div class="weather-forecast-date">` + day + `</div>
+          <img
+            src="http://openweathermap.org/img/wn/10d@2x.png"
+            alt=""
+            width="60"
+          />
+          <br />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-temperature-min">13°</span>|<span class="weather-temperature-max">14°</span>
+          </div>
+        </div>
+      `;
+  });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+
 
 //display forecast
 
-
-
-
-
-//display forecast
 
 
 function displayWeatherCondition(response) {
@@ -73,9 +97,12 @@ function showTemperature(response) {
   let iconElement = document.querySelector("#icon");
 
   h2.innerHTML = temperature + "° <br>" + weatherDescription;
+
   h4.innerHTML = "Wind: " + wind + "<br> Humidity: " + humidity + "%";
   iconElement.setAttribute("src", 'http://openweathermap.org/img/wn/' + response.data.weather[0].icon + '@2x.png');
 }
+
+displayForecast();
 
 function currentPosition(position) {
   let latitude = position.coords.latitude;
